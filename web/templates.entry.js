dc5e0fb924f929e2f245e7ce12c13b68d66dbb21
Brian Tawney <btawney@manjurist.net>
2020-08-18 16:21:34 +0000

    Added support for read-only entry

--- web/templates.entry.js
+++ web/templates.entry.js
@@ -14,6 +14,81 @@ templates.entry = function() {
   ]);
 };
 
+templates.transformText = function(t) {
+  var colon = t.indexOf(':');
+  if (colon >= 0) {
+    var textToUse = t.substr(colon + 1);
+  } else {
+    var textToUse = t;
+  }
+  return textToUse;
+};
+
+templates.entryReadOnly = function() {
+  return function() {
+    var binding = {
+      element: document.createElement('DIV'),
+      original: null,
+      setValue: function(v) {
+        var html = '';
+
+        if ('lemmas' in v) {
+          for (var i = 0; i < v.lemmas.length; ++i) {
+            var textToUse = templates.transformText(v.lemmas[i]);
+            html += (i > 0 ? ', ' : '') + '<b>' + textToUse + '</b>';
+          }
+        }
+
+        if ('paradigm' in v) {
+          html += ' (<i>' + v.paradigm.partOfSpeech + '</i>)';
+        }
+
+        if ('definitions' in v) {
+          if (v.definitions.length > 1) {
+            for (var i = 0; i < v.definitions.length; ++i) {
+              var def = v.definitions[i];
+              html += ' <b>' + (i + 1).toString() + '</b>. ' + def.definition + ';';
+
+              for (var j = 0; j < def.usages.length; ++j) {
+                html += ' ' + (i + 1).toString() + '.' + (j + 1).toString() + ' <i>' + def.usages[j].formula + '</i>, ' + def.usages[j].reading + ';';
+              }
+            }
+          } else if (v.definitions.length == 1) {
+            var def = v.definitions[0];
+            html += ' ' + def.definition;
+          }
+        }
+
+        if ('paradigm' in v) {
+          for (var code in v.paradigm.paradigm) {
+            if (v.paradigm.paradigm[code].length > 0) {
+              html += '; <b>' + code + '</b>';
+              var first = true;
+              for (var i = 0; i < v.paradigm.paradigm[code].length; ++i) {
+                if (first) {
+                  html += ': ';
+                  first = false;
+                } else {
+                  html += ', ';
+                }
+
+                html += templates.transformText(v.paradigm.paradigm[code][i]);
+              }
+            }
+          }
+        }
+
+        binding.element.innerHTML = html;
+      },
+      getValue: function() {
+        return binding.original;
+      }
+    };
+
+    return binding;
+  };
+};
+
 templates.entryParadigm = function() {
   return function() {
     var partsOfSpeech = data.setting.value('partsOfSpeech');
